b85674545e45b8fa9764f95d1692a90c70e86da8
skloczi <anna.maria.sklodowska@gmail.com>
2021-01-26 10:06:59 +0100

    added new functions

--- myfunctions/__init__.py
+++ myfunctions/__init__.py
@@ -1,3 +1,9 @@
 from .signal import deconvo
 from .signal import rm_damaged_signal
 from .signal import interfer_deconvo
+from .signal import rm_damaged_sensors
+from .signal import xcorr
+from .signal import stacked_fft
+from .signal import butter_bandpass, butter_lowpass
+
+from .geometry import intersensor_distance
