6340881802c02eac6de68269c5cc010ef60f49ef
skloczi <anna.maria.sklodowska@gmail.com>
2021-01-28 23:45:50 +0100

    minor change

--- myfunctions/geometry.py
+++ myfunctions/geometry.py
@@ -1,18 +1,19 @@
 import numpy as np
+import math
 
 # inter-sensor distance
-def intersensor_distance(ref_Nord, ref_East, coordinates_N, coordinates_E):
+def intersensor_distance(ref_North, ref_East, coordinates_N, coordinates_E):
     """ Calculates the intersensor distance from the reference station. Code
     valid for coordinates in [m].
 
     Parameters
     ----------
-    ref_Nord    :   float
-        Nord coordinates of the reference station
+    ref_North    :   float
+        North coordinates of the reference station
     ref_East    :   float
         East coordinates of the reference station
     coordinates_N   :   array_like
-        list of the Nord coordinates of the stations in the array_like
+        list of the North coordinates of the stations in the array_like
     coordinates_E   :   array_like
         list of the East coordinates of the stations in the array_like
     """
@@ -21,8 +22,8 @@ def intersensor_distance(ref_Nord, ref_East, coordinates_N, coordinates_E):
     distance = np.zeros(nb_sens)
 
     for sensor in range(nb_sens):
-        dNord = ref_Nord - coordinates_N[sensor]
+        dNord = ref_North - coordinates_N[sensor]
         dEast = ref_East - coordinates_E[sensor]
-        distance[sensor] = sqrt(dNord ** 2 + dEast ** 2)
+        distance[sensor] = math.sqrt(dNord ** 2 + dEast ** 2)
 
     return distance
