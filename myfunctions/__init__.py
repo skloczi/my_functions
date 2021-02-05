dbbb3f95f94e9a8b9cb9eb6a8627c146ae40a738
skloczi <anna.maria.sklodowska@gmail.com>
2021-02-05 23:38:28 +0100

    added moving window FFT

--- myfunctions/__init__.py
+++ myfunctions/__init__.py
@@ -3,7 +3,7 @@ from .signal import rm_damaged_signal
 from .signal import interfer_deconvo
 from .signal import rm_damaged_sensors
 from .signal import xcorr
-from .signal import stacked_fft
+from .signal import stacked_fft, moving_window_fft
 from .signal import butter_bandpass, butter_lowpass
 
 from .geometry import intersensor_distance
