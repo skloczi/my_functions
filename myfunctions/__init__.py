48da712c75a2d7e4d89f82202672ec2065ad3985
skloczi <anna.maria.sklodowska@gmail.com>
2021-02-18 02:42:17 +0100

    added freq_deconvo and butter_bandpass

--- myfunctions/__init__.py
+++ myfunctions/__init__.py
@@ -1,9 +1,9 @@
-from .signal import deconvo
+from .signal import deconvo, freq_deconvo
 from .signal import rm_damaged_signal
 from .signal import interfer_deconvo
 from .signal import rm_damaged_sensors
 from .signal import xcorr
 from .signal import stacked_fft, moving_window_fft
-from .signal import butter_bandpass, butter_lowpass
+from .signal import butter_bandpass, butter_lowpass, butter_highpass
 
 from .geometry import intersensor_distance
