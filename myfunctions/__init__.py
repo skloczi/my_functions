7235d4c3261639d9bbcafeb64b4a9f04aa798c3c
skloczi <anna.maria.sklodowska@gmail.com>
2021-02-24 14:20:51 +0100

    added konno_ohmachi smoothing fun

--- myfunctions/__init__.py
+++ myfunctions/__init__.py
@@ -7,3 +7,5 @@ from .signal import stacked_fft, moving_window_fft
 from .signal import butter_bandpass, butter_lowpass, butter_highpass
 
 from .geometry import intersensor_distance
+
+from .konno_ohmachi import fast_konno_ohmachi, faster_konno_ohmachi
